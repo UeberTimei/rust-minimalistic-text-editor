858ff3b2d85b5e77b8d6ae24a6b660384f900e5c
UeberTimei <super.timati04@gmail.com>
2023-12-05 13:55:41 +0300

    Added comments and guide to the project

---
+++ README.md
@@ -0,0 +1,31 @@
+# Rust Minimalistic Text Editor
+
+This project is a minimalistic text editor implemented in Rust.
+I have had inspiration from this [video](https://youtu.be/gnvDPCXktWQ?si=6c-Hqqedm7TGL6qQ) and decided to implement the same idea in Rust.
+
+## Overview
+
+This text editor provides basic functionality for editing and saving text files.
+
+## Features
+
+- Simple and intuitive interface
+- Basic text editing capabilities
+- File saving and loading
+
+## Usage
+
+To use the text editor, follow these steps:
+
+1. Clone the repository to your local machine. `git clone https://github.com/UeberTimei/rust-minimalistic-text-editor.git`
+2. Build the project using `cargo build`.
+3. Run the executable with `cargo run`.
+4. Use for example 'hello.txt', that lies in the directory.
+
+## Contributing
+
+Feel free to contribute to the development of this text editor by submitting pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
 use std::{io::{self, Read, Write}, fs::{OpenOptions, File}};
 
 fn main() -> Result<(), io::Error> {
+    // Open the file in read mode
     let mut file = OpenOptions::new().read(true).open("hello.txt").expect("There is no such file");
+
+    // Read the file content into a string buffer
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).unwrap();
-    drop(file);
+    drop(file); // Close the file
 
+    // Print the content of the file
     println!("Content: \n{}\n", buffer);
 
+    // Read user input for line number to modify
     let mut buf_num = String::new();
     let num = match io::stdin().read_line(&mut buf_num) {
         Ok(_) => buf_num.trim().parse::<usize>().unwrap(),
         Err(_) => 0,
     };
+
+    // Create a vector to hold each line of the file
     let mut new_data: Vec<String> = buffer.lines().map(|line| line.trim().to_string()).collect();
 
+    // Read user input for the new data to replace the specified line
     let mut change_to = String::new();
     io::stdin().read_line(&mut change_to).expect("An error occurred while reading new data");
+
+    // Update the specified line with the new data
     new_data[num] = change_to.trim().to_string();
 
+    // Create a new file or overwrite existing file with the modified data
     let mut changed_file = File::create("hello.txt").unwrap();
-    changed_file.write(new_data.join("\n").as_bytes()).expect("An error occured while writing new data");
-    drop(changed_file);
+    changed_file.write(new_data.join("\n").as_bytes()).expect("An error occurred while writing new data");
+    drop(changed_file); // Close the file
 
     Ok(())
 }
